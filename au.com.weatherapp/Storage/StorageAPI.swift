2d7f6ed89eb7641f63521de1c104296a35782a58
Nishant Gupta <nishantgupta@Nishants-MacBook-Pro.local>
2021-04-29 09:08:42 +1000

    documentation for storage api

--- au.com.weatherapp/Storage/StorageAPI.swift
+++ au.com.weatherapp/Storage/StorageAPI.swift
@@ -7,22 +7,41 @@
 
 import Foundation
 
+
+/// API to interact with device storage
 class StorageAPI {
     static let storeProvider: StoreProvider = StoreProviderImplementation()
     
+    
+    /// Method to Store string into a store
+    /// - Parameters:
+    ///   - value: value to be stored
+    ///   - key: key associated with data
     static func set(string value: String, forkey key: StorageKey) {
         StorageAPI.storeProvider.set(value: Data(value.utf8), forKey: key)
     }
     
+    
+    /// Method to Store Data into a store
+    /// - Parameters:
+    ///   - value: value to be stored
+    ///   - key: key associated with data
     static func set(data value: Data, forkey key: StorageKey) {
         StorageAPI.storeProvider.set(value: value, forKey: key)
     }
     
+    
+    /// Method to retrieve string value from storage based on key
+    /// - Parameter key: Key associated with data
+    /// - Returns: String value stored
     static func string(forkey key: StorageKey) -> String? {
         guard let data = StorageAPI.storeProvider.get(forKey: key) else { return nil }
         return String(data: data, encoding: .utf8)
     }
     
+    /// Method to retrieve data value from storage based on key
+    /// - Parameter key: Key associated with data
+    /// - Returns: Data value stored
     static func get(forkey key: StorageKey) -> Data? {
         return storeProvider.get(forKey: key)
     }
--- au.com.weatherapp/Storage/Store.swift
+++ au.com.weatherapp/Storage/Store.swift
@@ -8,9 +8,27 @@
 import Foundation
 
 protocol Store {
+    /// Method to check if particular store can be used for a key
+    /// - Parameter key: Key for Data to be stored/ retreived
     func isValid(forKey key: StorageKey) -> Bool
+    
+    /// Method to set store data into store based on key
+    /// - Parameters:
+    ///   - value: value to be stored
+    ///   - key: key  associated with data
     func set(value: Data, forKey key: StorageKey)
+    
+    
+    /// Method to retrieve data
+    /// - Parameter key: key associated with data
     func get(forKey key: StorageKey) -> Data?
+    
+    
+    /// Method to remove data based on key
+    /// - Parameter key: key associated with data
     func remove(forKey key: StorageKey)
+    
+    
+    /// Method ro remove all data from store
     func removeAll()
 }
